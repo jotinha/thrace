1e86a0acd2830901801759f34ed925966ca0f0b7
jotinha <jotinha@github>
2014-09-23 05:20:20 +0100

    simpler implementation using vvMap and vsMap

--- Core.hs
+++ Core.hs
@@ -8,11 +8,25 @@ import GHC.Exts (sortWith)
 
 data Vector = Vector Float Float Float deriving (Show,Eq)
 
-vnorm :: Vector -> Float
-vnorm v = sqrt $ v `dot` v
+vvMap :: (Float -> Float -> Float) -> Vector -> Vector -> Vector
+vvMap f (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (f x1 x2) (f y1 y2) (f z1 z2)
 
-vnormalize :: Vector -> Vector
-vnormalize v =  v ./ (vnorm v)
+vsMap :: (Float -> Float -> Float) -> Vector -> Float -> Vector
+vsMap f (Vector x1 y1 z1) s = Vector (f x1 s) (f y1 s) (f z1 s)
+
+(.+.) = vvMap (+)
+(.+) =  vsMap (+)
+(+.) =  (.+)
+(.-.) = vvMap (-)
+(.-) =  vsMap (-)
+(-.) s v = (Vector s s s) .-. v
+(./) = vsMap (/)
+(.*) = vsMap (*)
+(*.) = (.*)
+vmagnitude v = sqrt $ dot v v
+vnormalize v = v ./ (vmagnitude v)
+dot (Vector x1 y1 z1) (Vector x2 y2 z2) = x1*x2 + y1*y2 + z1*z2
+
 -- other
 
 minimumWith :: Ord b => (a -> b) -> [a] -> a
@@ -24,26 +38,26 @@ maximumWith f xs = last $ sortWith f xs
 limits  :: Ord t => t -> t -> t -> t
 limits minv maxv = (max minv) . (min maxv)
 
-class VectorOps a where
-  (.+.) :: a -> a -> a
-  (.-.) :: a -> a -> a
-  (.+)  :: a -> Float -> a
-  (+.)  :: Float -> a -> a
-  (.-)  :: a -> Float -> a
-  (-.)  :: Float -> a -> a
-  (./)  :: a -> Float -> a
-  (.*)  :: a -> Float -> a
-  (*.)  :: Float -> a -> a
-  dot   :: a -> a -> Float
+--class VectorOps a where
+--  (.+.) :: a -> a -> a
+--  (.-.) :: a -> a -> a
+--  (.+)  :: a -> Float -> a
+--  (+.)  :: Float -> a -> a
+--  (.-)  :: a -> Float -> a
+--  (-.)  :: Float -> a -> a
+--  (./)  :: a -> Float -> a
+--  (.*)  :: a -> Float -> a
+--  (*.)  :: Float -> a -> a
+--  dot   :: a -> a -> Float
 
-instance VectorOps Vector where
-  (Vector x1 y1 z1) .+. (Vector x2 y2 z2) = Vector (x1 + x2) (y1 + y2) (z1 + z2)
-  (Vector x1 y1 z1) .-. (Vector x2 y2 z2) = Vector (x1 - x2) (y1 - y2) (z1 - z2)
-  (Vector x y z) .+ s = Vector (x + s)  (y + s)  (z +s)
-  s +. v = v .+ s
-  v .- s = v .+ (-s)
-  s -. v = (Vector s s s) .-. v
-  (Vector x y z) .* s = Vector (x*s) (y*s) (z*s)
-  s *. v = v .* s
-  v ./ s = v .* (1/s)
-  dot (Vector x1 y1 z1) (Vector x2 y2 z2) = x1*x2 + y1*y2 + z1*z2
+--instance VectorOps Vector where
+--  (Vector x1 y1 z1) .+. (Vector x2 y2 z2) = Vector (x1 + x2) (y1 + y2) (z1 + z2)
+--  (Vector x1 y1 z1) .-. (Vector x2 y2 z2) = Vector (x1 - x2) (y1 - y2) (z1 - z2)
+--  (Vector x y z) .+ s = Vector (x + s)  (y + s)  (z +s)
+--  s +. v = v .+ s
+--  v .- s = v .+ (-s)
+--  s -. v = (Vector s s s) .-. v
+--  (Vector x y z) .* s = Vector (x*s) (y*s) (z*s)
+--  s *. v = v .* s
+--  v ./ s = v .* (1/s)
+--  dot (Vector x1 y1 z1) (Vector x2 y2 z2) = x1*x2 + y1*y2 + z1*z2
