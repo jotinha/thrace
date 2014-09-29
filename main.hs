62a1bf7a9c599558b83333bd8193c3451f2bbdeb
jotinha <jotinha@github>
2014-09-29 03:33:42 +0100

    fixed right handness

--- main.hs
+++ main.hs
@@ -14,7 +14,7 @@ pixel2World (imWidth,imHeight) i j = toWorld ( toScreen ( toNDC ( toRaster i j) 
     toRaster i j      = (fromIntegral j + 0.5, fromIntegral i + 0.5)
     toNDC    (x,y)    = (x/imWidth',y/imHeight')                 
     toScreen (nx,ny)  = ((2*nx - 1)*imAspRatio, - (2*ny - 1))    
-    toWorld  (sx,sy)  =  (sx, sy, 1)
+    toWorld  (sx,sy)  =  (sx, sy, -1)
 
 makeImage :: Resolution -> World -> [Color]
 makeImage res@(width,height) world = [
@@ -45,13 +45,15 @@ writePPM (width,height) pixels  | width*height /= length pixels = error "Invalid
 
 myWorld = World {
   objects = [
-    Object (Sphere (Vector3 0 0  20) 5 ) (Color 0.2 0.2 0.8) 0 0 "sphere1",
-    Object (Sphere (Vector3 0 5  30) 5 ) (Color 0.8 0.2 0.8) 0 0 "sphere1b"
-    --Object (Sphere (Vector3 0 (-30) 20) 20 ) (Color 1 0 0) 0 1 "sphere2",
-    --Object (Sphere (Vector3 (-20) 0 20) 5 ) (Color 0 0.8 0.0) 0 1 "sphere3",
-    --Object (Sphere (Vector3 0 40 100) 5 ) (Color 0 0.4 0.0) 0 1 "sphere4",
-    --Object (Sphere (Vector3 8 8 25 ) 5 ) (Color 0.6 0.5 1.0) 0 1 "sphere5",
-    --Object (Plane  (Vector3 0 1 0) (7) )  (Color 0.95 0.95 0.8) 0 0 "plane"
+    --Object (Sphere (Vector3 0 0  (-20)) 5 ) (Color 0.2 0.2 0.8) 0 0 "sphere1",
+    --Object (Sphere (Vector3 0 5  (-30)) 5 ) (Color 0.8 0.2 0.8) 0 0 "sphere1b",
+    --Object (Sphere (Vector3 0 (-30) (-20)) 20 ) (Color 1 0 0) 0 1 "sphere2",
+    --Object (Sphere (Vector3 (-20) 0 (-20)) 5 ) (Color 0 0.8 0.0) 0 1 "sphere3",
+    --Object (Sphere (Vector3 0 40 (-100)) 5 ) (Color 0 0.4 0.0) 0 1 "sphere4",
+    --Object (Sphere (Vector3 8 8 (-25)) 5 ) (Color 0.6 0.5 1.0) 0 1 "sphere5",
+    Object (Plane  (Vector3 0 1 0) (7) )  (Color 0.95 0.95 0.8) 0 0 "plane",
+    Object (Triangle (Vector3 0 0 (-20)) (Vector3 (10) 0 (-20)) (Vector3 (10) (1 0) (-30))) 
+           (Color 0.5 0.1 0.1) 0 0 "tri1"
   ],
   lights = [
     Light (Vector3 20 100 0) (Color 1 1 1) 0.5
