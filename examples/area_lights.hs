047b2fec62182e6c2baad652522edc2a786de7ed
jotinha <jotinha@github>
2014-11-25 00:48:57 +0000

    changed example for comparison

--- examples/area_lights.hs
+++ examples/area_lights.hs
@@ -6,13 +6,13 @@ myWorld = World {
       0 0 "room"
     ),
  
-    (Object (Sphere (Vector3 (0) 0 (-5)) 2) (Color 0.5 0.5 0.5) 1 0 "sphere1"),
+    (Object (Sphere (Vector3 (0) 0 (-4.5)) 2) (Color 0.5 0.5 0.5) 1 1 "sphere1"),
     (Object (Sphere (Vector3 (-2) 0 (-3)) 1) (Color 1 0 0) 0 0 "sphere2"),
-    (Object (Sphere (Vector3 (4) 0 (-8)) 3) (Color 1 0 0) 0 0 "sphere3")
+    (Object (Sphere (Vector3 (4) 0 (-8.1)) 3) (Color 1 0 0) 0 0 "sphere3")
   ],
   lights = [
     --Light (PointLight (Vector3 (0) 4 (-2))) (Color 20 20 20),
-    Light (GeomLight (Sphere (Vector3 0 4 (1)) 0.5)) (Color 30 30 30)
+    Light (GeomLight (Sphere (Vector3 3 3 (0)) 0.5)) (Color 30 30 30)
   ],
   backgroundColor = Color 0 0 0
 }
